7d9db0419a8496ae3d3f8b5281aee508b74e721a
mahdilamb <mahdilamb@gmail.com>
2022-09-19 13:56:54 +0100

    add csv reader

--- main.cpp
+++ main.cpp
@@ -21,10 +21,24 @@ int main(int argc, char *argv[])
 {
   int cost_max = 1000;
   bool hide_matrix = false;
+  bool hide_assignments = false;
+  std::string filepath;
   int dim = 4;
 
+  cost **assigncost, *u, *v, lapcost;
+  int i, *colsol;
+  int j, *rowsol;
   ArgConsumer arg_consumer{"LAP.JV usage"};
   arg_consumer
+      .add_argument(
+          "--file",
+          [&dim, &assigncost, &filepath](char *path)
+          {
+            filepath = path;
+            dim = read_csv<cost>(assigncost, path, ',', [](const std::string &value)
+                                 { return std::stoi(value); });
+          },
+          "Path to a file to use for calculating LAP.")
       .add_argument(
           "--dim",
           [&dim](char *new_dim)
@@ -36,20 +50,25 @@ int main(int argc, char *argv[])
           { hide_matrix = true; },
           "If present will not print the cost matrix", true)
       .add_argument(
+          "--hide-assignments",
+          [&hide_assignments](char *ignored)
+          { hide_assignments = true; },
+          "Whether to hide the assignments", true)
+      .add_argument(
           "--cost-max",
           [&cost_max](char *new_cost_range)
           { cost_max = stoi(std::string(new_cost_range)); },
           "The upper limit of the random numbers used for the cost matrix")
       .consume(argc, argv);
 
-  cost **assigncost, *u, *v, lapcost;
-  int i, *colsol;
-  int j, *rowsol;
+  if (!filepath.size())
+  {
+    assigncost = new cost *[dim];
+    for (i = 0; i < dim; i++)
+      assigncost[i] = new cost[dim];
+    fill_random<cost>(assigncost, dim, cost_max);
+  }
 
-  assigncost = new cost *[dim];
-  for (i = 0; i < dim; i++)
-    assigncost[i] = new cost[dim];
-  fill_random<cost>(assigncost, dim, cost_max);
   rowsol = new int[dim];
   colsol = new int[dim];
   u = new cost[dim];
@@ -68,12 +87,15 @@ int main(int argc, char *argv[])
     highlighted_print<cost>(const_cast<const cost **>(assigncost), dim, idx);
     std::cout << std::endl;
   }
-  std::cout << "Assignments: " << std::endl;
-  std::cout << " col -> row" << std::endl;
-
-  for (int i = 0; i < dim; ++i)
+  if (!hide_assignments)
   {
-    std::cout << std::setw(4) << i << " -> " << colsol[i] << std::endl;
+    std::cout << "Assignments: " << std::endl;
+    std::cout << " col -> row" << std::endl;
+
+    for (int i = 0; i < dim; ++i)
+    {
+      std::cout << std::setw(4) << i << " -> " << colsol[i] << std::endl;
+    }
   }
 
   checklap<int>(dim, assigncost, rowsol, colsol, u, v);
